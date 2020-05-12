f47a925ed5dc06f46f239e2baf988fa035eda4e6
Amer Fahmy <afahmy519@gmail.com>
2020-05-12 20:28:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,10 +13,10 @@ end
 
 end
 
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+emails1 = "codecode.com, food.org, netflix.com"
 parser1 = EmailAddressParser.new(emails1)
 parser1.parse 
 
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+emails2 = "codecode.com food.org per@er.org netflix.com"
 parser2 = EmailAddressParser.new(emails2)
 parser2.parse
