a571260e30c523556680a5a4924c544bce382302
Amer Fahmy <afahmy519@gmail.com>
2020-05-12 20:34:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,9 +14,9 @@ end
 end
 
 #emails1 = "codecode.com, food.org, netflix.com"
-parser1 = EmailAddressParser.new(emails1)
-parser1.parse 
+#parser1 = EmailAddressParser.new(emails1)
+#parser1.parse 
 
-emails2 = "codecode.com food.org per@er.org netflix.com"
-parser2 = EmailAddressParser.new(emails2)
+#emails2 = "codecode.com food.org per@er.org netflix.com"
+#parser2 = EmailAddressParser.new(emails2)
 parser2.parse
