420589725e1d79b71c5d643bb864064c762be57b
Amer Fahmy <afahmy519@gmail.com>
2020-05-12 20:24:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,9 +1,9 @@
 def parse 
     csv_emails.split.collect do |address|
       address.split(',') 
-    
+    end
     .flatten.uniq 
-  end 
+  
 end
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
